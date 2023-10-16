ad80006a0d73fdf11d58d4263ffecd72d53a2764
Anthony Roussel <anthony@roussel.dev>
2023-10-16 10:43:31 +0200

    xfce terminal

--- modules/xfce/hm.nix
+++ modules/xfce/hm.nix
@@ -1,5 +1,14 @@
 { pkgs, config, lib, ... }@args:
 
+let
+  colors = config.lib.stylix.colors.withHashtag;
+
+  themeFile = config.lib.stylix.colors {
+    template = ./terminalrc.mustache;
+    extension = "theme";
+  };
+
+in
 {
   options.stylix.targets.xfce.enable =
     config.lib.stylix.mkEnableTarget "Xfce" true;
@@ -14,5 +23,18 @@
         "Gtk/MonospaceFontName" = "${monospace.name} ${toString sizes.applications}";
       };
     };
+
+
+    home.file.".local/share/xfce4/terminal/colorschemes/stylix.theme".source = themeFile;
+
+    xdg.configFile."xfce4/terminal/terminalrc".text = with config.stylix.fonts; ''
+      [Configuration]
+      ColorForeground=${colors.base05}
+      ColorBackground=${colors.base00}
+      ColorCursor=x
+      ColorBoldIsBright=FALSE
+      ColorPalette=${colors.base01};${colors.base08};${colors.base0B};${colors.base0A};${colors.base0D};${colors.base0F};${colors.base0C};${colors.base06};${colors.base00};${colors.base09};${colors.base02};${colors.base03};${colors.base04};${colors.base0E};${colors.base05};${colors.base07}
+      FontName=${monospace.name} ${toString sizes.applications}
+    '';
   };
 }
